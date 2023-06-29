28c4dbba779a2e570fa30170ed154669e369ee33
Tom Milligan <tom.milligan@uipath.com>
2023-06-29 15:24:51 +0100

    web: make buttons gradient :sparkle:

--- qvet-web/src/components/Theme.tsx
+++ qvet-web/src/components/Theme.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import {
+  Theme as MuiTheme,
+  ThemeProvider,
+  createTheme,
+  CSSObject,
+  Palette,
+  PaletteColor,
+} from "@mui/material/styles";
 
 export const ColorModeContext = React.createContext({
   toggleColorMode: () => {},
@@ -7,11 +14,14 @@ export const ColorModeContext = React.createContext({
 
 type ThemeMode = "light" | "dark";
 
-const systemIsDarkTheme = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
+const systemIsDarkTheme =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 export default function Theme({ children }: { children: React.ReactNode }) {
-  const storedMode = (localStorage.getItem("theme_mode") ??
-    systemIsDarkTheme ?  "dark" : "light") as ThemeMode;
+  const storedMode = (
+    localStorage.getItem("theme_mode") ?? systemIsDarkTheme ? "dark" : "light"
+  ) as ThemeMode;
   const [mode, setMode] = React.useState<ThemeMode>(storedMode);
   const colorMode = React.useMemo(
     () => ({
@@ -26,11 +36,21 @@ export default function Theme({ children }: { children: React.ReactNode }) {
     []
   );
 
+  const baseTheme = React.useMemo(() => createTheme(), [createTheme]);
+  const gradientButtonOverrides = makeGradientButtonOverrides(baseTheme);
+  console.log(gradientButtonOverrides);
   const theme = React.useMemo(
     () =>
       createTheme({
         palette: {
           mode,
+        },
+        components: {
+          MuiButton: {
+            styleOverrides: {
+              ...gradientButtonOverrides,
+            },
+          },
         },
       }),
     [mode]
@@ -41,4 +61,40 @@ export default function Theme({ children }: { children: React.ReactNode }) {
       <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </ColorModeContext.Provider>
   );
+}
+
+function assertPaletteColor(
+  paletteColor: any
+): asserts paletteColor is PaletteColor {
+  if (paletteColor.main === undefined) {
+    throw new Error("assertPaletteColor");
+  }
+}
+
+interface StyleOverrides {
+  [key: string]: CSSObject;
+}
+function makeGradientButtonOverrides(theme: MuiTheme): object {
+  const styleOverrides: StyleOverrides = {};
+  const variants: Array<keyof Palette> = [
+    "primary",
+    "secondary",
+    "success",
+    "info",
+    "error",
+    "warning",
+  ];
+  variants.forEach((variant: keyof Palette) => {
+    const paletteColor = theme.palette[variant];
+    assertPaletteColor(paletteColor);
+    const key = "contained" + capitalizeFirstLetter(variant);
+    styleOverrides[key] = {
+      background: `linear-gradient(45deg, ${paletteColor.dark} 10%, ${paletteColor.main} 30%, ${paletteColor.light} 90%)`,
+    };
+  });
+  return styleOverrides;
+}
+
+function capitalizeFirstLetter(value: string): string {
+  return value.charAt(0).toUpperCase() + value.slice(1);
 }
