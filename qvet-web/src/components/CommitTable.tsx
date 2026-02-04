82b6c87957e542644e407b6cc0a2fc9bb569cda4
Tom Milligan <tom.milligan@uipath.com>
2026-02-04 10:28:49 +0000

    fix(web): increase max table to 200

--- qvet-web/src/components/CommitTable.tsx
+++ qvet-web/src/components/CommitTable.tsx
@@ -1,13 +1,18 @@
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
 
 import CommitRow from "src/components/CommitRow";
 import { Commit } from "src/octokitHelpers";
 
 import TableHeader from "./TableHeader";
+
+const MAX_COMMITS_IN_TABLE = 200;
 
 export default function CommitTable({
   commits: rawCommits,
@@ -16,11 +21,24 @@ export default function CommitTable({
   commits: ReadonlyArray<Commit>;
   showHeader: boolean;
 }): React.ReactElement {
-  // FIXME warn/paginate on large numbers
-  const commits = rawCommits.slice(0, 100);
+  // FIXME: paginate on large numbers
+  const commits = rawCommits.slice(0, MAX_COMMITS_IN_TABLE);
+  const showTooManyCommitsError = commits.length !== rawCommits.length;
 
   return (
     <>
+      {showTooManyCommitsError && (
+        <Alert severity="error">
+          <AlertTitle>Too many commits</AlertTitle>
+          <Typography>
+            There were too many commits in this batch to show.
+          </Typography>
+          <Typography>
+            Some commits are missing. Please raise qvet's limit, or implement
+            pagination.
+          </Typography>
+        </Alert>
+      )}
       {showHeader && <TableHeader title="Commits" />}
       <Table sx={{ minWidth: 650 }} size="small">
         <TableHead>
